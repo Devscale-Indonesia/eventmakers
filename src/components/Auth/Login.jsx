801edb513b5978bdc3ccaa4cfd85205cdfcddae0
Indra Zulfi <me@indrazm.com>
2023-09-19 21:21:44 +0700

    feat: adding auth functionality

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,15 +1,40 @@
 "use client";
 
 import { Button, Input } from "@nextui-org/react";
+import { useState } from "react";
 
 export const Login = () => {
+  const initialLoginData = {
+    email: "",
+    password: "",
+  };
+
+  const [loginData, setLoginData] = useState(initialLoginData);
+
+  const handleEventChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+
+  const handleLogin = async () => {
+    const { email, password } = loginData;
+    const res = await fetch("https://eventmakers-api.vercel.app/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <main className="h-screen flex justify-center items-center">
       <div className="w-[260px] space-y-2">
         <h1>Login</h1>
-        <Input name="email" placeholder="email@something.com" />
-        <Input name="password" type="password" placeholder="password" />
-        <Button color="primary">Login</Button>
+        <Input name="email" placeholder="email@something.com" onChange={handleEventChange} />
+        <Input name="password" type="password" placeholder="password" onChange={handleEventChange} />
+        <Button color="primary" onClick={handleLogin}>
+          Login
+        </Button>
       </div>
     </main>
   );
--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,16 +1,41 @@
 "use client";
 
 import { Button, Input } from "@nextui-org/react";
+import { useState } from "react";
 
 export const Register = () => {
+  const initialRegisterData = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  const [registerData, setRegisterData] = useState(initialRegisterData);
+
+  const handleEventChange = (e) => {
+    const { name, value } = e.target;
+    setRegisterData({ ...registerData, [name]: value });
+  };
+
+  const handleRegister = async () => {
+    const { name, email, password } = registerData;
+    const res = await fetch("https://eventmakers-api.vercel.app/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <main className="h-screen flex justify-center items-center">
       <div className="w-[260px] space-y-2">
         <h1>Register</h1>
-        <Input name="name" placeholder="full name" />
-        <Input name="email" placeholder="email@something.com" />
-        <Input name="password" type="password" placeholder="password" />
-        <Button color="primary">Register</Button>
+        <Input name="name" placeholder="full name" onChange={handleEventChange} />
+        <Input name="email" placeholder="email@something.com" onChange={handleEventChange} />
+        <Input name="password" type="password" placeholder="password" onChange={handleEventChange} />
+        <Button color="primary" onClick={handleRegister}>
+          Register
+        </Button>
       </div>
     </main>
   );
