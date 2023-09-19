e8242d13d2b8a95a01a1dc57414f81fb9aa56156
Indra Zulfi <me@indrazm.com>
2023-09-19 20:42:17 +0700

    chore: adding next ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import { Login } from "@/components/auth/Login";
+
 export default function Page() {
-  return <div>Hello!</div>;
+  return <Login />;
 }
---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+
+export const Login = () => {
+  return (
+    <div>
+      <Button color="primary">Hello!</Button>
+    </div>
+  );
+};
